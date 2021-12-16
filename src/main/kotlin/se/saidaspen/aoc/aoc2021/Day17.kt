ec4bb089bc8f62db8198a4f95b25a2c23f3f269a
Said Aspen <git@saidaspen.se>
2021-12-16 17:23:07 +0100

    Added up to day 17 - template

---
+++ src/main/kotlin/se/saidaspen/aoc/aoc2021/Day17.kt
@@ -0,0 +1,25 @@
+package se.saidaspen.aoc.aoc2021
+
+import se.saidaspen.aoc.util.Day
+import se.saidaspen.aoc.util.StringTokenizer
+import se.saidaspen.aoc.util.toBinary
+import java.lang.RuntimeException
+
+fun main() = Day17.run()
+
+object Day17 : Day(2021, 17) {
+
+    override fun part1(): Any {
+        println(input)
+        return ""
+    }
+
+    override fun part2(): Any {
+        return ""
+    }
+}
+
+
+
+
+
