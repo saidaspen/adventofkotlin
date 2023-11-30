8472d64400fbdd0e1ff94ef54d7ae9b819f4f764
Said Aspen <git@saidaspen.se>
2023-11-30 15:54:36 +0100

    Smaller changes after work on DB Loader

---
+++ src/main/kotlin/se/saidaspen/aoc/aoc2022/Day20.kt
@@ -0,0 +1,19 @@
+package se.saidaspen.aoc.aoc2022
+
+import se.saidaspen.aoc.util.*
+import java.lang.Integer.max
+import kotlin.math.min
+
+fun main() = Day20.run()
+
+object Day20 : Day(2022, 20) {
+
+    override fun part1(): Any {
+        println(input)
+        return ""
+    }
+
+    override fun part2(): Any {
+        return ""
+    }
+}
