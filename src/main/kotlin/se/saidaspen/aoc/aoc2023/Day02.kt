eb339a89d29aefd17ceac4094e92dc35b894c02f
Said Aspen <mail@saidaspen.se>
2023-12-02 06:22:31 +0100

    2023 02

--- src/main/kotlin/se/saidaspen/aoc/aoc2023/Day02.kt
+++ src/main/kotlin/se/saidaspen/aoc/aoc2023/Day02.kt
@@ -5,14 +5,32 @@ import se.saidaspen.aoc.util.*
 fun main() = Day02.run()
 
 object Day02 : Day(2023, 2) {
+
     override fun part1() : Any {
-        println(input)
-        return 0
+        var possible = 0
+        for (line in input.lines()) {
+            val gameID = words(line)[1].replace(":", "").toInt()
+            val game = line.split(":")[1]
+            val reds = words(game).windowed(2).filter { it[1].startsWith("red") }.maxOf { it[0].toInt() }
+            val greens = words(game).windowed(2).filter { it[1].startsWith("green") }.maxOf { it[0].toInt() }
+            val blues = words(game).windowed(2).filter { it[1].startsWith("blue") }.maxOf { it[0].toInt() }
+            if (reds <= 12 && greens <= 13 && blues <= 14)
+                possible += gameID
+        }
+        return possible
     }
 
     override fun part2() : Any {
-        println(input)
-        return 0
+        var sum = 0
+        for (line in input.lines()) {
+            val game = line.split(":")[1]
+            val reds = words(game).windowed(2).filter { it[1].startsWith("red") }.maxOf { it[0].toInt() }
+            val greens = words(game).windowed(2).filter { it[1].startsWith("green") }.maxOf { it[0].toInt() }
+            val blues = words(game).windowed(2).filter { it[1].startsWith("blue") }.maxOf { it[0].toInt() }
+            val power = reds*greens*blues
+            sum +=power
+        }
+        return sum
     }
 }
 
---
+++ src/main/kotlin/se/saidaspen/aoc/aoc2023/Day03.kt
@@ -0,0 +1,20 @@
+package se.saidaspen.aoc.aoc2023
+
+import se.saidaspen.aoc.util.*
+
+fun main() = Day03.run()
+
+object Day03 : Day(2023, 3) {
+
+    override fun part1() : Any {
+        println(input)
+        return 0
+    }
+
+    override fun part2() : Any {
+        return 0
+    }
+}
+
+
+
