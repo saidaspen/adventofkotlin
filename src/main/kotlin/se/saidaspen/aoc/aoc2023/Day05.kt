606b46d9a8d4244922770e53ade4599dd0fd02e2
Said Aspen <mail@saidaspen.se>
2023-12-05 11:24:20 +0100

    2023 03 05

--- src/main/kotlin/se/saidaspen/aoc/aoc2023/Day05.kt
+++ src/main/kotlin/se/saidaspen/aoc/aoc2023/Day05.kt
@@ -1,7 +1,6 @@
 package se.saidaspen.aoc.aoc2023
 
 import se.saidaspen.aoc.util.*
-import kotlin.math.max
 import kotlin.math.min
 
 fun main() = Day05.run()
@@ -28,43 +27,32 @@ object Day05 : Day(2023, 5) {
         return minLoc
     }
 
-    private fun translateRange(map: Map<LongRange, LongRange>, input: LongRange): List<LongRange> {
-        val outputRanges = mutableListOf<LongRange>()
-        var unmappedRange = mutableListOf(input)
-        for (srcRange in map.keys) {
-            val outsideRanges = mutableListOf<LongRange>()
-            for (toMapRange in unmappedRange) {
-                // Stuff that is outside (lower) than the mapping
-                if (toMapRange.first < srcRange.first)
-                    outsideRanges.add(toMapRange.first .. min(toMapRange.last, srcRange.first - 1))
-                // Stuff that is outside (larger) than the mapping
-                if (toMapRange.last > srcRange.last)
-                    outsideRanges.add(max(srcRange.last + 1, toMapRange.first) .. toMapRange.last)
-                // Stuff that is in the mappers range
-                if (toMapRange.first <= srcRange.last && toMapRange.last >= srcRange.first) {
-                    val insideRange = max(srcRange.first, toMapRange.first) .. min(srcRange.last, toMapRange.last)
-                    val translation = map[srcRange]!!.first - srcRange.first
-                    outputRanges.add(insideRange.first + translation .. insideRange.last + translation)
-                }
-            }
-            unmappedRange = outsideRanges
-        }
-        outputRanges.addAll(unmappedRange)
-        return outputRanges
-    }
-
     override fun part2(): Any {
         val seedRanges = longs(input.lines()[0]).chunked(2).map { it[0] .. it[0]+it[1] }
         val maps = input.split("\n\n").drop(1).map { section ->
-            section.lines()
-                .drop(1) // This is the header
+            section.lines().drop(1)
                 .associate { longs(it).let { (dst, src, len) -> src..(src + len) to dst..(dst + len) } }
         }
-        return seedRanges.flatMap { seed ->
-            maps.fold(listOf(seed)) { acc, map -> acc.flatMap { translateRange(map, it) } }
+        return seedRanges.flatMap {
+            seed -> maps.fold(listOf(seed)) { acc, map -> acc.flatMap { translateRange(map, it) } }
         }.minOf { it.first }
     }
+
+    private fun translateRange(map: Map<LongRange, LongRange>, input: LongRange): List<LongRange> {
+        val outputRanges = mutableListOf<LongRange>()
+        var unmappedRanges = mutableListOf(input)
+        for (srcRange in map.keys) {
+            val outsideRanges = mutableListOf<LongRange>()
+            for (toMapRange in unmappedRanges) {
+                outsideRanges.addAll(toMapRange.exceptRange(srcRange))
+                toMapRange.intersectRange(srcRange)?.let {
+                    outputRanges.add(it.shift(map[srcRange]!!.first - srcRange.first))
+                }
+            }
+            unmappedRanges = outsideRanges
+        }
+        outputRanges.addAll(unmappedRanges)
+        return outputRanges
+    }
 }
-
-
 
---
+++ src/main/kotlin/se/saidaspen/aoc/util/Ranges.kt
@@ -0,0 +1,23 @@
+package se.saidaspen.aoc.util
+
+import kotlin.math.max
+import kotlin.math.min
+
+fun LongRange.exceptRange(srcRange: LongRange): List<LongRange> {
+    val below = if (this.first < srcRange.first) this.first ..min(this.last, srcRange.first - 1) else null
+    val above = if (this.last > srcRange.last) max(srcRange.last + 1, this.first).. this.last else null
+    return listOfNotNull(below, above)
+}
+fun LongRange.shift(steps: Long) = this.first + steps .. this.last + steps
+fun LongRange.intersectRange(that: LongRange) : LongRange? = if (this.first <= that.last && this.last >= that.first) max(that.first, this.first) .. min(that.last, this.last) else null
+
+fun IntRange.exceptRange(srcRange: IntRange): List<IntRange> {
+    val below = if (this.first < srcRange.first) this.first .. min(this.last, srcRange.first - 1) else null
+    val above = if (this.last > srcRange.last) max(srcRange.last + 1, this.first).. this.last else null
+    return listOfNotNull(below, above)
+}
+
+fun IntRange.shift(steps: Int) = this.first + steps .. this.last + steps
+fun IntRange.intersectRange(that: IntRange) : IntRange? = if (this.first <= that.last && this.last >= that.first) max(that.first, this.first)..min(that.last, this.last) else null
+
+
