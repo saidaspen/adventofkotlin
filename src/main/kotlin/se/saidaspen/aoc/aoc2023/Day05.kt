dfc1625df74152a1bcdd42c8d1f224eaf9e4e15a
Said Aspen <mail@saidaspen.se>
2023-12-05 10:34:43 +0100

    2023 03 05

--- src/main/kotlin/se/saidaspen/aoc/aoc2023/Day05.kt
+++ src/main/kotlin/se/saidaspen/aoc/aoc2023/Day05.kt
@@ -28,67 +28,41 @@ object Day05 : Day(2023, 5) {
         return minLoc
     }
 
-    override fun part2(): Any {
-        val seedRanges = longs(input.lines()[0]).windowed(2, 2).map { P(it[0], it[0]+it[1]) }
-        val mapraw = input.split("\n\n").drop(1)
-        val maps = mutableMapOf<P<String, String>, List<P<P<Long, Long>, P<Long, Long>>>>()
-        for (m in mapraw) {
-            val lines = m.lines()
-            val name = lines[0].replace(" map:", "").split("-")
-            val mapname = P(name[0], name[2])
-            val map = mutableListOf<P<P<Long, Long>, P<Long, Long>>>()
-            for (l in 1 until lines.size) {
-                val nums = longs(lines[l])
-                val srcrngstart = nums[1]
-                val dstrngstart = nums[0]
-                val rnglen = nums[2]
-                map.add(P(P(srcrngstart, srcrngstart + rnglen), P(dstrngstart, dstrngstart + rnglen)))
+    private fun translateRange(map: Map<LongRange, LongRange>, input: LongRange): List<LongRange> {
+        val outputRanges = mutableListOf<LongRange>()
+        var unmappedRange = mutableListOf(input)
+        for (srcRange in map.keys) {
+            val outsideRanges = mutableListOf<LongRange>()
+            for (toMapRange in unmappedRange) {
+                // Stuff that is outside (lower) than the mapping
+                if (toMapRange.first < srcRange.first)
+                    outsideRanges.add(toMapRange.first .. min(toMapRange.last, srcRange.first - 1))
+                // Stuff that is outside (larger) than the mapping
+                if (toMapRange.last > srcRange.last)
+                    outsideRanges.add(max(srcRange.last + 1, toMapRange.first) .. toMapRange.last)
+                // Stuff that is in the mappers range
+                if (toMapRange.first <= srcRange.last && toMapRange.last >= srcRange.first) {
+                    val insideRange = max(srcRange.first, toMapRange.first) .. min(srcRange.last, toMapRange.last)
+                    val translation = map[srcRange]!!.first - srcRange.first
+                    outputRanges.add(insideRange.first + translation .. insideRange.last + translation)
+                }
             }
-            maps[mapname] = map
+            unmappedRange = outsideRanges
         }
-        var minLoc = Long.MAX_VALUE
+        outputRanges.addAll(unmappedRange)
+        return outputRanges
+    }
 
-        fun map(job: P<P<String, String>, P<Long, Long>>) : List<P<P<String, String>, P<Long, Long>>> {
-            val mapper = maps[job.first]!!
-            val outputRanges = mutableListOf<P<Long, Long>>()
-            var notMappedRanges = mutableListOf(job.second)
-            for (range in mapper) {
-                val outsideRanges = mutableListOf<P<Long, Long>>()
-                for (toMapRange in notMappedRanges) {
-                    if (toMapRange.first < range.first.first) {
-                        outsideRanges.add(P(toMapRange.first, min(toMapRange.second, range.first.first - 1)))
-                    }
-                    if (toMapRange.second > range.first.second) {
-                        outsideRanges.add(P(max(range.first.second + 1, toMapRange.first), toMapRange.second))
-                    }
-                    if (toMapRange.first <= range.first.second && toMapRange.second >= range.first.first) {
-                        val insideRange = P(max(range.first.first, toMapRange.first), min(range.first.second, toMapRange.second))
-                        val translation = range.second.first - range.first.first
-                        val translatedRange = P(insideRange.first + translation, insideRange.second + translation)
-                        outputRanges.add(translatedRange)
-                    }
-                }
-                notMappedRanges = outsideRanges
-            }
-            outputRanges.addAll(notMappedRanges)
-            var needle = maps.keys.firstOrNull { it.first == job.first.second }
-            if (needle == null) {
-                needle = Pair("location", "")
-            }
-            return outputRanges.map { P(needle, it) }
+    override fun part2(): Any {
+        val seedRanges = longs(input.lines()[0]).chunked(2).map { it[0] .. it[0]+it[1] }
+        val maps = input.split("\n\n").drop(1).map { section ->
+            section.lines()
+                .drop(1) // This is the header
+                .associate { longs(it).let { (dst, src, len) -> src..(src + len) to dst..(dst + len) } }
         }
-        val jobList = seedRanges.map { P(P("seed", "soil"), it) }.toMutableList()
-        while (jobList.isNotEmpty()) {
-            val job = jobList.removeFirst()
-            if (job.first.second == "") {
-                if (job.second.first < minLoc) {
-                    minLoc = job.second.first
-                }
-            } else {
-                jobList.addAll(0, map(job))
-            }
-        }
-        return minLoc
+        return seedRanges.flatMap { seed ->
+            maps.fold(listOf(seed)) { acc, map -> acc.flatMap { translateRange(map, it) } }
+        }.minOf { it.first }
     }
 }
 
---
+++ src/main/kotlin/se/saidaspen/aoc/aoc2023/Day06.kt
@@ -0,0 +1,23 @@
+package se.saidaspen.aoc.aoc2023
+
+import se.saidaspen.aoc.util.*
+import kotlin.math.max
+import kotlin.math.min
+
+fun main() = Day06.run()
+
+object Day06 : Day(2023, 6) {
+
+    override fun part1(): Any {
+        println(input)
+        return 0
+    }
+
+    override fun part2(): Any {
+        println(input)
+        return 0
+    }
+}
+
+
+
